b9ecf60010f99d1748cc7ea86caebf55ca20cd25
realsep <58202232+realsep@users.noreply.github.com>
2020-01-16 14:06:12 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = 5004;
+var rp = require('request-promise');
+
+
+var app = new express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get('/', function(req, res) {
+    res.sendFile('/search.html', { root: __dirname });
+    //     res.send("complete!");
+
+});
+
+app.post('/test', function(req, res) {
+    var options = {
+        method: 'POST',
+        uri: ' https://80beac66.ngrok.io/test',
+        json: true
+    };
+
+    rp(options)
+        .then(function(parsedBody) {
+            res.send(parsedBody)
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+});
+
+app.listen(process.env.PORT || port, function() {
+    console.log('Node start on port : ' + port);
+});
